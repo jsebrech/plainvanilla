64fefaaa26a8d283b9e5988f5065c96ee626b508
Joeri Sebrechts <joeri@sebrechts.net>
2024-08-30 20:20:22 +0200

    add analytics

--- public/components/analytics/analytics.js
+++ public/components/analytics/analytics.js
@@ -20,6 +20,16 @@ class AnalyticsComponent extends HTMLElement {
     update() {
         if (this.isConnected && this.#template) {
             this.innerHTML = this.#template;
+            // replace scripts by executable versions
+            const scripts = this.getElementsByTagName('script');
+            for (const script of scripts) {
+                const newScript = document.createElement('script');
+                for (const attr of script.attributes) {
+                    newScript.setAttribute(attr.name, attr.value);
+                }
+                newScript.innerHTML = script.innerHTML;
+                script.replaceWith(newScript);
+            }
         }
     }
 }
