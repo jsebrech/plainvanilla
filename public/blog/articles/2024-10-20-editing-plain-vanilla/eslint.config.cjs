d1bd748e1cd33f726f1a05bde5b8d677574e5536
Joeri Sebrechts <joeri@sebrechts.net>
2024-10-20 14:59:49 +0200

    publish article

---
+++ public/blog/articles/2024-10-20-editing-plain-vanilla/eslint.config.cjs
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+const globals = require("globals");
+const js = require("@eslint/js");
+
+module.exports = [
+    js.configs.recommended, 
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.mocha
+            },
+            ecmaVersion: 2022,
+            sourceType: "module",
+        }
+    },
+    {
+        ignores: [
+            "public/blog/articles/",
+            "**/lib/",
+            "**/react/",
+        ]
+    }
+];
